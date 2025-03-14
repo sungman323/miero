9f8fd7a2725234727a424a31face7819166b1053
man323 <sungman.park@gmail.com>
2025-03-14 15:18:24 +0900

    first

---
+++ sub_event_h.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>미에로화이바 메인페이지</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+  <link rel="stylesheet" href="http://man323.dothome.co.kr/250311/css/reset.css" type="text/css">
+  <link rel="stylesheet" href="http://man323.dothome.co.kr/250311/css/base.css" type="text/css">
+  <link rel="stylesheet" href="http://man323.dothome.co.kr/250311/css/common.css" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+
+  <link rel="stylesheet" href="http://man323.dothome.co.kr/250311/css/sub.css" type="text/css">
+</head>
+<body>
+  <header>
+    <h1>
+      <a href="http://man323.dothome.co.kr/250311/index.html" title="메인페이지로 바로가기">
+        <img src="http://man323.dothome.co.kr/250311/images/logo2.png" alt="상단로고">
+      </a>
+    </h1>
+
+    <div id="toggle">
+      <span>&nbsp;</span>
+      <span>&nbsp;</span>
+      <span>&nbsp;</span>
+    </div>
+
+    <nav class="gnb">
+      <ul>
+        <li><a href="#" title="미에로">미에로 <i class="fas fa-angle-down"></i></a>
+          <ul class="sub">
+            <li><a href="#" title="브랜드 스토리">브랜드 스토리</a></li>
+            <li><a href="#" title="식이섬유 스토리">식이섬유 스토리</a></li>
+          </ul>
+        </li>
+        <li><a href="#" title="제품소개">제품소개 <i class="fas fa-angle-down"></i></a>
+          <ul class="sub">
+            <li><a href="#" title="미에로화이바">미에로화이바</a></li>
+            <li><a href="#" title="미에로화이바 스파클링 제로">미에로화이바 스파클링 제로</a></li>
+            <li><a href="#" title="헬씨올리고">헬씨올리고</a></li>
+            <li><a href="#" title="헬씨올리고 프로바이오틱스">헬씨올리고 프로바이오틱스</a></li>
+          </ul>
+        </li>
+        <li><a href="#" title="미에로 스토리">미에로 스토리 <i class="fas fa-angle-down"></i></a>
+          <ul class="sub">
+            <li><a href="#" title="TV광고">TV광고</a></li>
+            <li><a href="#" title="인쇄광고">인쇄광고</a></li>
+            <li><a href="#" title="프로모션">프로모션</a></li>
+          </ul>
+        </li>
+        <li><a href="#" title="미에로 이벤트">미에로 이벤트 <i class="fas fa-angle-down"></i></a>
+          <ul class="sub">
+            <li><a href="http://man323.dothome.co.kr/bbs/bbs/board.php?bo_table=mi_event" title="진행중인 이벤트">진행중인 이벤트</a></li>
+            <li><a href="#" title="당첨자 발표">당첨자 발표</a></li>
+          </ul>
+        </li>
+        <li><a href="http://man323.dothome.co.kr/bbs/bbs/board.php?bo_table=mi_notice" title="공지사항">공지사항</a></li>
+        <li><a href="https://hyundaipharmshop.com/" title="공식쇼핑몰"><i class="fa-solid fa-bag-shopping"></i> 공식쇼핑몰</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main>
+    <section class="con">
+      <div class="title_back"></div>
+      <h2>콘텐츠 영역</h2>
+      <nav class="lnb">
+        <ul>
+          <li><a href="http://man323.dothome.co.kr/250311/index.html" title="홈으로 가기">HOME</a></li>
+          <li>미에로 이벤트</li>
+          <li>진행중인 이벤트</li>
+        </ul>
+      </nav>
+
+      <article>
---
+++ sub_f2.php
@@ -0,0 +1,64 @@
+  <footer>
+    <div class="f_inner">
+      <nav>
+        <dl>
+          <dt><a href="#" title="미에로">미에로</a></dt>
+          <dd><a href="#" title="브랜드 스토리">브랜드 스토리</a></dd>
+          <dd><a href="#" title="식이섬유 스토리">식이섬유 스토리</a></dd>
+        </dl>
+        <dl>
+          <dt><a href="#" title="제품소개">제품소개</a></dt>
+          <dd><a href="#" title="미에로화이바">미에로화이바</a></dd>
+          <dd><a href="#" title="미에로화이바 SPARKLING ZERO SUGAR">미에로화이바 SPARKLING ZERO SUGAR</a></dd>
+          <dd><a href="#" title="헬씨올리고">헬씨올리고</a></dd>
+        </dl>
+        <dl>
+          <dt><a href="#" title="미에로 스토리">미에로 스토리</a></dt>
+          <dd><a href="#" title="TV광고">TV광고</a></dd>
+          <dd><a href="#" title="인쇄광고">인쇄광고</a></dd>
+          <dd><a href="#" title="프로모션">프로모션</a></dd>
+        </dl>
+        <dl>
+          <dt><a href="#" title="미에로 이벤트">미에로 이벤트</a></dt>
+          <dd><a href="http://man323.dothome.co.kr/bbs/bbs/board.php?bo_table=mi_event" title="진행중인 이벤트">진행중인 이벤트</a></dd>
+          <dd><a href="#" title="당첨자 발표">당첨자 발표</a></dd>
+        </dl>
+        <dl>
+          <dt><a href="http://man323.dothome.co.kr/bbs/bbs/board.php?bo_table=mi_notice" title="공지사항">공지사항</a></dt>
+          <dd><a href="http://man323.dothome.co.kr/bbs/bbs/board.php?bo_table=mi_notice" title="News&Notice">News&amp;Notice</a></dd>
+        </dl>
+        <dl>
+          <dt><a href="#" title="현대약품">현대약품</a></dt>
+          <dd>&nbsp;</dd>
+        </dl>
+        <dl>
+          <dt>Connect with us</dt>
+          <dd>
+            <a href="#" title="페이스북"><i class="fab fa-facebook-f"></i></a>
+            <a href="#" title="인스타그램"><i class="fab fa-instagram"></i></a>
+            <a href="#" title="유튜브"><i class="fab fa-youtube"></i></a>
+          </dd>
+        </dl>
+      </nav>
+    </div>
+    <a href="#" title="위로 바로가기"><img src="http://man323.dothome.co.kr/250311/images/top_up.png" alt="위로 바로가기"></a>
+    <address>
+      <h2><a href="http://man323.dothome.co.kr/250311/index.html" title="메인페이지로 바로가기"><img src="http://man323.dothome.co.kr/250311/images/logo2.png" alt="하단로고"></a></h2>
+      <p>copyright&copy; 2025 MIERO FIBER. All Rights Reserved.</p>
+    </address>
+  </footer>
+
+  <script>
+    $(function(){
+      const mnu = $('.gnb > ul > li > a');
+      $(mnu).click(function(){
+        $(this).parent().siblings().find('.sub').hide();
+        $(this).next().toggle();
+      });
+      $('main').click(function(){
+        $('.sub').hide();
+      });
+    });
+  </script>
+</body>
+</html>
